827771eb44aa112b19a31ab397013bd59e59743e
Kremerin <110621622+kremerin@users.noreply.github.com>
2022-10-06 18:47:20 +0200

    Vers1

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,22 +8,22 @@ let eingabe = document.getElementById("eingabe");
 
 
 function rechner() {
-    if (nettoBrutto.checked && (hoch.checked)) {
+    if (nettoBrutto.checked && hoch.checked) {
         let bruttoMwstRech = eingabe.value * 0.19;
         let bruttoRech = eingabe.value * 1.19;
         mwst.innerHTML = "Mehrwertsteuerbetrag: " + bruttoMwstRech.toFixed(2) + " €";
         brutto.innerHTML = "Bruttobetrag (Endpreis): " + bruttoRech.toFixed(2) + " €";
-    } else if (nettoBrutto.checked && (niedrig.checked)) {
+    } else if (nettoBrutto.checked && niedrig.checked) {
         let bruttoMwstRech = eingabe.value * 0.07;
         let bruttoRech = eingabe.value * 1.07;
         mwst.innerHTML = "Mehrwertsteuerbetrag: " + bruttoMwstRech.toFixed(2) + " €";
         brutto.innerHTML = "Bruttobetrag (Endpreis): " + bruttoRech.toFixed(2) + " €";
-    } else if (bruttoNetto.checked && (hoch.checked)) {
+    } else if (bruttoNetto.checked && hoch.checked) {
         let nettoMwstRech = eingabe.value / 119 * 19;
         let nettoRech = eingabe.value / 1.19;
         mwst.innerHTML = "Mehrwertsteuerbetrag: " + nettoMwstRech.toFixed(2) + " €";
         netto.innerHTML = "Nettobetrag (Endpreis): " + nettoRech.toFixed(2) + " €";
-    } else if (bruttoNetto.checked && (niedrig.checked)) {
+    } else if (bruttoNetto.checked && niedrig.checked) {
         let nettoMwstRech = eingabe.value / 107 * 7;
         let nettoRech = eingabe.value / 1.07;
         mwst.innerHTML = "Mehrwertsteuerbetrag: " + nettoMwstRech.toFixed(2) + " €";
